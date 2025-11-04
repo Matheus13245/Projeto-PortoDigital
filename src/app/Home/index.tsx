3f5db3b9fcd6404ee0cda228051afc0b451b6329
Matheus13245 <matheus.costa0020016102@edu.pe.senac.br>
2025-11-04 14:50:23 -0300

    teste1

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Teste</Text>
     </View>
   );
 }
