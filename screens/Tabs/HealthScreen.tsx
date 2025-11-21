796793edec1d8e875bcf26451dcbcdef5491e4af
Matheus13245 <matheus.costa0020016102@edu.pe.senac.br>
2025-11-21 13:14:57 -0300

    Add files via upload

---
+++ screens/Tabs/HealthScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+export default function HealthScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Saúde/Telemetria (placeholder)</Text>
+    </View>
+  );
+}
